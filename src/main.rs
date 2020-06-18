88d245a3e7cfdb4b9ec6d2a3f12bc741e00f2727
Sahil Tembhare <isr.sahil.mk@gmail.com>
2020-06-18 14:40:41 +0530

    zzz

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,44 @@
 extern crate reqwest;
 
-use reqwest::Client;
-
 use std::io::{self, BufRead};
 use std::error;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn error::Error>> {
     let mut subs_vec = Vec::new();
+    let text_vec: Vec<String> = env::args().skip(1).collect();
+
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         subs_vec.push(line.unwrap());
     }
 
-    for sub in subs_vec {
-        println!("{}", sub);
-        client(&sub).await?;
+    if text_vec.is_empty() {
+        eprintln!("Please provide title to check webpage for.\nUsage: cat domains.txt | checkr \"Webpage Title 1\" \"Webpage Title 2\"")
+    } else {
+        for sub in subs_vec {
+            client(&sub, &text_vec).await?;
+        }
     }
 
     Ok(())
 
 }
 
-async fn client(sub: &str) -> Result<(), reqwest::Error> {
+async fn client(sub: &str, vec: &Vec<String>) -> Result<(), reqwest::Error> {
     let client = reqwest::Client::builder()
         .danger_accept_invalid_certs(true)
         .build()?;
 
     let res = client.get(sub).send().await?;
-    println!("Status: {}", res.status());
     let body = res.text().await?;
-    println!("Body:\n\n{}", body);
+
+    for text in vec {
+        if !body.contains(text) {
+            println!("{}", sub);
+        }
+    }
 
     Ok(())
 }
