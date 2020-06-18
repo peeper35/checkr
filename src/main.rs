34316b4ab17e0c737a4cf5b189c7db5c486af19d
Sahil Tembhare <isr.sahil.mk@gmail.com>
2020-06-18 13:41:14 +0530

    created reqwest cleint for making requests

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
-use std::io::{self, BufRead};
+extern crate reqwest;
 
-fn main() {
+use reqwest::Client;
+
+use std::io::{self, BufRead};
+use std::error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn error::Error>> {
     let mut subs_vec = Vec::new();
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         subs_vec.push(line.unwrap());
     }
 
+    for sub in subs_vec {
+        println!("{}", sub);
+        client(&sub).await?;
+    }
+
+    Ok(())
+
+}
+
+async fn client(sub: &str) -> Result<(), reqwest::Error> {
+    let client = reqwest::Client::builder()
+        .danger_accept_invalid_certs(true)
+        .build()?;
+
+    let res = client.get(sub).send().await?;
+    println!("Status: {}", res.status());
+    let body = res.text().await?;
+    println!("Body:\n\n{}", body);
+
+    Ok(())
 }
